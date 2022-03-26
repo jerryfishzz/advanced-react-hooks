6483a439aa2cc16b9c458f85e5267c72e966526e
jerryfishzz <jerryfish@163.com>
2022-03-27 11:36:44 +1300

    Complete 1-1
    feat: complete 1-1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,6 +3,7 @@
 
 import * as React from 'react'
 
+/* 
 // Exercise
 function countReducer(prevCount, newCount) {
   return newCount
@@ -23,6 +24,22 @@ function Counter({initialCount = 0, step = 1}) {
   const increment = () => setCount(count + step)
   return <button onClick={increment}>{count}</button>
 }
+ */
+
+
+// Extra 1
+function countReducer(prevCount, step) {
+  return prevCount + step
+}
+
+// Extra 1
+function Counter({initialCount = 0, step = 1}) {
+  const [count, changeCount] = React.useReducer(countReducer, initialCount)
+
+  const increment = () => changeCount(step)
+  return <button onClick={increment}>{count}</button>
+}
+
 
 function App() {
   return <Counter />
